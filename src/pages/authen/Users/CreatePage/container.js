752f857a99ea01b8e60f33a49df634731a25e930
Oscar <dtpham258@gmail.com>
2018-12-11 09:11:18 +0700

    update

--- src/pages/authen/Users/CreatePage/container.js
+++ src/pages/authen/Users/CreatePage/container.js
@@ -1,21 +1,72 @@
 import { getList, changeStatus,createUser } from 'reducers/user'
 import { getList as getGroupList }  from 'reducers/group'
 
+changeStepStatus = (current) => {
+  for (let step of this.props.steps) {
+    if (step.index < current) {
+      step.icon = step.iconDefault
+      step.status = 'finish'
+    } else if (step.index === current) {
+      if (step.index !== this.props.steps.length - 1) {
+        step.icon = 'loading'
+      }
+      step.status = 'process'
+    } else {
+      step.icon = step.iconDefault
+      step.status = 'wait'
+    }
+  }
+}
 
 export const mapDispathToProps = {
   getList: (limit, page, sort, isAsc) => getList(limit, page, sort, isAsc),
   getGroupList: (limit, page, sort, isAsc) => getGroupList(limit, page, sort, isAsc),
   changeStatus: (id, status) => changeStatus(id, status),
-  createUser: (model)=> createUser(model)
+  createUser: (model)=> createUser(model),
+  changeStepStatus: (current)=> changeStepStatus(current)
 }
 export const mapStateToProps = (state, props) => {
 
+  const steps = [{
+    title: 'Adding details',
+    subTitle: 'Adding User Information',
+    icon: 'loading',
+    iconDefault: 'user',
+    status: 'process',
+    index: 0,
+    nextTitle: 'Next: Permission',
+  }, {
+    title: 'Set permission',
+    subTitle: 'Set Permission for User',
+    icon: 'solution',
+    iconDefault: 'solution',
+    status: 'wait',
+    index: 1,
+    nextTitle: 'Next: Review',
+  }, {
+    title: 'Review',
+    subTitle: 'Review',
+    icon: 'menu-unfold',
+    iconDefault: 'menu-unfold',
+    status: 'wait',
+    index: 2,
+    nextTitle: 'Create User',
+  }, {
+    title: 'Done',
+    subTitle: 'Create User Complete',
+    icon: 'check-circle',
+    iconDefault: 'check-circle',
+    status: 'wait',
+    index: 3,
+    nextTitle: 'Go to Users List',
+  },];
   return {
     totalItems: state.user.totalItems,
     page: state.user.page,
     data: state.user.users,
     userCreate: state.user.userCreate || {},
-    groups : state.group.groups
+    groups : state.group.groups,
+    steps: steps
   }
 }
 
--- src/pages/authen/Users/CreatePage/index.js
+++ src/pages/authen/Users/CreatePage/index.js
@@ -13,39 +13,6 @@ const Step = Steps.Step;
 const ButtonGroup = Button.Group;
 
 
-const steps = [{
-  title: 'Adding details',
-  subTitle: 'Adding User Information',
-  icon: 'loading',
-  iconDefault: 'user',
-  status: 'process',
-  index: 0,
-  nextTitle: 'Next: Permission',
-}, {
-  title: 'Set permission',
-  subTitle: 'Set Permission for User',
-  icon: 'solution',
-  iconDefault: 'solution',
-  status: 'wait',
-  index: 1,
-  nextTitle: 'Next: Review',
-}, {
-  title: 'Review',
-  subTitle: 'Review',
-  icon: 'menu-unfold',
-  iconDefault: 'menu-unfold',
-  status: 'wait',
-  index: 2,
-  nextTitle: 'Create User',
-}, {
-  title: 'Done',
-  subTitle: 'Create User Complete',
-  icon: 'check-circle',
-  iconDefault: 'check-circle',
-  status: 'wait',
-  index: 3,
-  nextTitle: 'Go to Users List',
-},];
 
 @connect(
   mapStateToProps,
@@ -73,7 +40,7 @@ class UserCreatePage extends React.Component {
     const current = this.state.step.current + index;
     console.log('current', current)
     if (current < steps.length) {
-      this.changeStatus(current)
+      this.props.changeStepStatus(current)
       this.setState({
         step: {
           current: current
@@ -85,26 +52,11 @@ class UserCreatePage extends React.Component {
       this.setState({ link: '#/users' })
     }
   }
-  changeStatus = (current) => {
-    for (let step of steps) {
-      if (step.index < current) {
-        step.icon = step.iconDefault
-        step.status = 'finish'
-      } else if (step.index === current) {
-        if (step.index !== steps.length - 1) {
-          step.icon = 'loading'
-        }
-        step.status = 'process'
-      } else {
-        step.icon = step.iconDefault
-        step.status = 'wait'
-      }
-    }
-  }
+
 
   render() {
-    const { current } = this.state.step;
-    console.log(current)
+    const { current } = this.state.step
+
     return (
       <div className='user-create'>
         <div className='card'>
@@ -113,48 +65,41 @@ class UserCreatePage extends React.Component {
               <div className='utils__title'>
                 <h3 className='font-weight-bold'>CREATE A NEW USER</h3>
               </div>
-              <Divider orientation='left'><h4>Step {steps[current].index + 1}: {steps[current].title}</h4></Divider>
+              <Divider orientation='left'><h4>Step {this.props.steps[current].index + 1}: {this.props.steps[current].title}</h4></Divider>
             </div>
 
           </div>
           <div className='card-body'>
             <Steps current={current}>
-              {steps.map(item => <Step key={item.title} title={item.title} status={item.status} icon={<Icon type={item.icon} />} />)}
+              {this.props.steps.map(item => <Step key={item.title} title={item.title} status={item.status} icon={<Icon type={item.icon} />} />)}
             </Steps>
           </div>
         </div>
         <div className='card'>
           <div className='card-header'>
             <h4 className='text-black mb-3'>
-              <strong>{steps[current].subTitle}</strong>
+              <strong>{this.props.steps[current].subTitle}</strong>
             </h4>
           </div>
           <div className='card-body'>
-            {this.state.step.current === 0 && (<Step1
-              userCreate={this.props.userCreate}
-              createUser={this.props.createUser} />)}
-            {this.state.step.current === 1 && (<Step2
-              userCreate={this.props.userCreate}
-              createUser={this.props.createUser}
-              getGroupList={this.props.getGroupList} />)}
-            {(this.state.step.current === 2
-              || this.state.step.current === 3
-            ) && (<Review />)}
+            {this.state.step.current === 0 && (<Step1/>)}
+            {this.state.step.current === 1 && (<Step2 />)}
+            {(this.state.step.current === 2 || this.state.step.current === 3) && (<Review />)}
             <div className='col-md-12 text-right'>
               <ButtonGroup className=''>
                 <Button href='#/users'>Cancel</Button>
-                {(current > 0 && current < steps.length - 1) && (<Button
+                {(current > 0 && current < this.props.steps.length - 1) && (<Button
                   type='default'
                   onClick={() => this.changeStep(-1)}
                 >
                   <Icon type='left' /> Previous
                 </Button>)}
-                {(current < steps.length) && (<Button className='text-white'
+                {(current < this.props.steps.length) && (<Button className='text-white'
                   type='primary'
                   onClick={() => this.changeStep(1)}
                   href={this.state.link}
                 >
-                  {steps[current].nextTitle}<Icon type='right' />
+                  {this.props.steps[current].nextTitle}<Icon type='right' />
                 </Button>)}
               </ButtonGroup>
             </div>
