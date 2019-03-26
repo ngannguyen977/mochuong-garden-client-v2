b99ce2278b8c35163b9459d1993b29a13d4be5b3
Your Name <dtpham258@gmail.com>
2019-03-26 17:43:11 +0700

    fix

--- src/pages/iot/things/ListPage/index.js
+++ src/pages/iot/things/ListPage/index.js
@@ -8,6 +8,7 @@ import helper from "../../../../helper"
 import "../../../../resources/style.scss"
 import "../style.scss"
 import ThingCard from "../../../components/ThingCard"
+import Quagga from 'quagga'
 
 const Search = Input.Search
 
@@ -43,19 +44,58 @@ class ListPage extends React.Component {
       current: page,
     })
   }
-
+  componentDidMount(){
+    Quagga.init({
+      inputStream : {
+        name : "Live",
+        type : "LiveStream",
+        target: document.querySelector('#scanner')    // Or '#yourElement' (optional)
+      },
+      decoder : {
+        readers : ["code_128_reader"]
+      }
+    }, function(err) {
+        if (err) {
+            console.log(err);
+            return
+        }
+        console.log("Initialization finished. Ready to start");
+        Quagga.start();
+    });
+  }
+scan = ()=>{
+  ScanditSDK.configure("xxx", {
+    engineLocation: "https://unpkg.com/scandit-sdk/build/"
+        }).then(() => {
+          console.log('Loaded');
+          ScanditSDK.BarcodePicker.create(document.getElementById('barcode-picker'), {
+            playSoundOnScan: true,
+            vibrateOnScan: true
+          }).then(function(barcodePicker) {
+            console.log("Ready");
+            barcodePicker.applyScanSettings(new ScanditSDK.ScanSettings({
+              enabledSymbologies: ["ean8", "ean13", "upca", "upce", "code128", "code39", "code93", "itf", "qr"],
+              codeDuplicateFilter: 1000
+            }));
+            barcodePicker.onScan(function(barcodes) {
+              console.log(barcodes);
+            });
+          });
+        });
+}
   render() {
     const { totalItems, destroy, data, type, history } = this.props
 
     return (
-      <div className="thing">
-        <section className="card">
-          <div className="card-header">
-            <div className="row">
-              <div className="col-md-10">
-                <div className="utils__title">
+      <div className='thing'>
+        <section className='card'>
+          <div className='card-header'>
+            <div className='row'>
+              <div className='col-md-10'>
+                <div className='utils__title'>
                   <strong>Things Management</strong>
                 </div>
+                <div id='scanner'></div>
                 <small>
                   Thing management allow admins can control all things. Administrators can create a
                   new thing, add a thing to several groups, attach some permission, change status or
@@ -63,24 +103,24 @@ class ListPage extends React.Component {
                   thing.
                 </small>
               </div>
-              <div className="col-md-2 thing__btn-create">
-                <Button type="primary" onClick={() => history.push("/things/register")}>
+              <div className='col-md-2 thing__btn-create'>
+                <Button type='primary' onClick={() => history.push("/things/register")}>
                   Register Thing
                 </Button>
               </div>
             </div>
           </div>
-          <div className="card-body">
-            <div className="operation-box">
-              <div className="search-box">
+          <div className='card-body'>
+            <div className='operation-box'>
+              <div className='search-box'>
                 <Search
-                  placeholder="input search text"
+                  placeholder='input search text'
                   onSearch={value => this.onChange(this.state.current, value)}
                   // onBlur={value => this.onChange(this.state.current, value)}
                   style={{ width: 600 }}
                 />
               </div>
-              <div className="text-right" style={{ marginBottom: 10 }}>
+              <div className='text-right' style={{ marginBottom: 10 }}>
                 <Pagination
                   current={this.state.current}
                   onChange={page => this.onChange(page, this.state.keyword)}
@@ -90,10 +130,10 @@ class ListPage extends React.Component {
               </div>
             </div>
             {totalItems > 0 && (
-              <div className="row">
+              <div className='row'>
                 {data &&
                   data.map(x => (
-                    <div className="col-md-2" key={x.name}>
+                    <div className='col-md-2' key={x.name}>
                       <ThingCard
                         data={x || {}}
                         type={type}
@@ -103,7 +143,7 @@ class ListPage extends React.Component {
                       />
                     </div>
                   ))}
-                <div className="col-md-12 text-right">
+                <div className='col-md-12 text-right'>
                   <Pagination
                     current={this.state.current}
                     onChange={this.onChange}
@@ -113,7 +153,7 @@ class ListPage extends React.Component {
                 </div>
               </div>
             )}
-            {totalItems <= 0 && <LockScreenPage name="Thing" link="#/things/register" />}
+            {totalItems <= 0 && <LockScreenPage name='Thing' link='#/things/register' />}
           </div>
         </section>
       </div>
