d4a3077e82af805ac63e4f9bf58d251387a1ed4f
Your Name <dtpham258@gmail.com>
2019-03-26 09:11:36 +0700

    user detail thing

--- src/pages/components/ThingCard/index.js
+++ src/pages/components/ThingCard/index.js
@@ -1,37 +1,38 @@
 import React from "react"
+import TimeAgo from "react-timeago"
+import helper from "../../../helper"
+import { Tag, Button, Tooltip, Popconfirm, message, Icon, Checkbox } from "antd"
 import "./style.scss"
-import data from "./data.json"
-import { Checkbox } from "antd"
 
 class ProductCard extends React.Component {
   state = {
-    productImg: data.productImg,
-    productStatus: data.productStatus,
+    productImg: "resources/iot/camera-onsky.png",
+    productStatus: "new",
   }
 
   render() {
     const { productImg, productStatus } = this.state
 
-    const { data, isEdit, permission, action } = this.props
+    const { data, isEdit, permission, action, remove, destroy } = this.props
     return (
-      <div className="thingCard">
-        <div className="thingCard__img">
+      <div className='thingCard'>
+        <div className='thingCard__img'>
           {productStatus === "new" && (
-            <div className="thingCard__status">
-              <span className="thingCard__status__title">active</span>
+            <div className='thingCard__status'>
+              <span className='thingCard__status__title'>active</span>
             </div>
           )}
           <a href={"/#/things/" + data.name}>
-            <img className="img-responsive" src={data.imageUrl || productImg} alt="" />
+            <img className='img-responsive' src={data.imageUrl || productImg} alt='' />
           </a>
         </div>
-        <div className="thingCard__title">
+        <div className='thingCard__title'>
           <a href={"/#/things/" + data.name}>{data.displayName}</a>
-          <div className="thingCard__price" />
+          <div className='thingCard__price' />
         </div>
-        <div className="thingCard__descr">{data.serial || "no description"}</div>
+        <div className='thingCard__descr'>{data.serial || "no description"}</div>
         {isEdit && (
-          <div className="thingCard__btn-control">
+          <div className='thingCard__btn-control'>
             <Checkbox
               defaultChecked={(permission || {}).isControl}
               onChange={e => action(data, e.target.checked)}
@@ -46,6 +47,66 @@ class ProductCard extends React.Component {
             </Checkbox>
           </div>
         )}
+        <div className='btn-group text-center'>
+          {action && <Tooltip
+            placement='bottom'
+            title='Toggle to set permission view this thing for this user.'
+          >
+            <Popconfirm
+              title='Are you sure change state of permission VIEW THING?'
+              onConfirm={() => action(data, !data.isView)}
+              onCancel={() => message.info("cancel selected!")}
+              okText='Yes'
+              cancelText='No'
+            >
+              <Tag color={data.isView ? "#52c41a" : "#d9d9d9"}>View</Tag>
+            </Popconfirm>
+          </Tooltip>}
+          {action && <Tooltip
+            placement='bottom'
+            title='Toggle to set permission control this thing for this user.'
+          >
+            <Popconfirm
+              title='Are you sure change state of permission CONTROL THING?'
+              onConfirm={() => action(data, !data.isControl)}
+              onCancel={() => message.info("cancel selected!")}
+              okText='Yes'
+              cancelText='No'
+            >
+              <Tag color={data.isControl ? "#2f54eb" : "#d9d9d9"}>Control</Tag>
+            </Popconfirm>
+          </Tooltip>}
+          {remove && (
+            <Tooltip placement='bottom' title='Click to remove this user from this thing.'>
+              <Popconfirm
+                title='Are you sure remove this user from this thing?'
+                onConfirm={() => remove(data.uuid)}
+                onCancel={() => message.info("cancel selected!")}
+                okText='Yes'
+                cancelText='No'
+              >
+                <Icon type='delete' />
+              </Popconfirm>
+            </Tooltip>
+          )}
+          {/* for user page */}
+          {destroy && <Tooltip
+            placement='bottom'
+            title='Toggle to status for this user.'
+          >
+            <Popconfirm
+              title={`Are you sure delete of user ${data.username}? You cannot rollback!`}
+              onConfirm={() => destroy('DELETE', data.username)}
+              onCancel={() => message.info("cancel selected!")}
+              okText='Yes'
+              cancelText='No'
+            >
+              <Button className='style-icon' type='danger' size='small' icon='delete'>
+                Delete
+                  </Button>
+            </Popconfirm>
+          </Tooltip>}
+        </div>
       </div>
     )
   }
