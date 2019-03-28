239f1dfad7bc3372f11de4426a1236451bdeeb41
Tho Pham <tho_pham@onskyinc.com>
2019-03-28 13:49:19 +0700

    remove unuse barcode

--- src/services/barcodeScanner.js
+++ src/services/barcodeScanner.js
@@ -23,7 +23,7 @@ export const app = {
                 target: document.querySelector('#scanner')    // Or '#yourElement' (optional)
             },
             decoder: {
-                readers: ['code_128_reader', 'ean_reader', 'ean_8_reader', 'code_39_reader', 'code_39_vin_reader', 'codabar_reader', 'upc_reader', 'upc_e_reader', 'i2of5_reader', '2of5_reader', 'code_93_reader']
+                readers: ['code_128_reader']
             }
         }, function (err) {
             if (err) {
@@ -63,7 +63,6 @@ export const app = {
     },
     onProcessed: function () {
         return Quagga.onProcessed(function (result) {
-            console.log('onProcessed', result)
             let drawingCtx = Quagga.canvas.ctx.overlay,
                 drawingCanvas = Quagga.canvas.dom.overlay
 
