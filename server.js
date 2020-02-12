e4afd3db213ead51fe3afdd8c2400bdefedb3d30
Phạm Nguyên Cát <dtpham258@gmail.com>
2020-02-12 10:03:49 +0700

    fix route server side

--- server.js
+++ server.js
@@ -7,5 +7,8 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 console.log('Server starting on port 8080')
 app.listen(8080);
